79b622e91d3781cd46d8611c7b7da7c0bf7f17b6
y-ohta <y-ohta@administraotr-no-MacBook-Air.local>
2016-01-28 19:32:38 +0900

    進めた

--- accessor.rb
+++ accessor.rb
@@ -3,9 +3,10 @@ require "net/ssh/gateway"
 require "mysql2"
 require "active_record"
 
+@db = YAML.load_file('./database.yml')
 contents = YAML.load_file('./contents.yml')
 adcords = YAML.load_file('./adcords.yml')
-date_range = range = Time.new(2015, 12, 1, 0, 0, 0)..Time.new(2015, 12, 31, 23, 59, 59)
+@date_range = range = Time.new(2015, 12, 1, 0, 0, 0)..Time.new(2015, 12, 31, 23, 59, 59)
 
 adcode_uids = adcords
 
@@ -25,7 +26,6 @@ class Ppv < ActiveRecord::Base
 end
 
 def getPaymentsFromPpv(uids)
-  db = YAML.load_file('./database.yml')
   gateway = Net::SSH::Gateway.new('nrdb-01.gbnet', 'root', :port => 22, :password => ENV["ROOT_PASSWD"])
   if !gateway.active?
     p "connection failed"
@@ -33,9 +33,9 @@ def getPaymentsFromPpv(uids)
   end
 
   gateway.open("localhost", 3306) {|local_port|
-    db["nrdb-01.gbnet"]["port"] = local_port
-    db["nrdb-01.gbnet"]["database"] = "haru"
-    ActiveRecord::Base.establish_connection(db["nrdb-01.gbnet"])
+    @db["nrdb-01.gbnet"]["port"] = local_port
+    @db["nrdb-01.gbnet"]["database"] = "haru"
+    ActiveRecord::Base.establish_connection(@db["nrdb-01.gbnet"])
 
 =begin
     uid = "https://id.my.softbank.jp/service/idp/server.php/idpage?id=e4sb2boukr3r7crsa5lj8sarkcb1r34b3e676m16i6du1t9t9f0i2doah4nefstj"
@@ -50,7 +50,7 @@ def getPaymentsFromPpv(uids)
     #q = q.or(q.where(uid: uid))
   }
   #uid_sel = q.matches(uid)
-  ret = Ppv.where(uid_sel).pluck(:menuid)
+  ret = Ppv.where(startdate: @date_range).where(uid_sel).pluck(:menuid)
   puts ret.length
 =begin
     q = Ppv.where(uid: "")
@@ -64,22 +64,19 @@ def getPaymentsFromPpv(uids)
 end
 
 gateway.open("localhost", 3306) {|local_port|
-  db = YAML.load_file('./database.yml')
-  db["sadb-01"]["port"] = local_port
-  db["sadb-01"]["database"] = "swan_analyze"
-  ActiveRecord::Base.establish_connection(db["sadb-01"])
+  @db = YAML.load_file('./database.yml')
+  @db["sadb-01"]["port"] = local_port
+  @db["sadb-01"]["database"] = "swan_analyze"
+  ActiveRecord::Base.establish_connection(@db["sadb-01"])
 
   adcode_uids = {}
   adcords.each {|adcord|
     adcode_uids[adcord] = []
-    q = AdcodeSubscribeLasts.where(date: date_range).where("adcode1 like ?", adcord)
+    q = AdcodeSubscribeLasts.where(date: @date_range).where("adcode1 like ?", adcord)
     adcode_uids[adcord] = q.pluck(:uid)
   }
   #puts adcode_uids["pr=menulistad"].length
+  getPaymentsFromPpv(adcode_uids["pr=menulistad"])
 }
 
 gateway.shutdown!
-
-uids = ["https://id.my.softbank.jp/service/idp/server.php/idpage?id=e4sb2boukr3r7crsa5lj8sarkcb1r34b3e676m16i6du1t9t9f0i2doah4nefstj",
-        "https://connect.auone.jp/net/id/hny_rt_net/cca/s/XBg1dJnhlDs1Ngwg/Xcmri4AY_IntcfBL"]
-getPaymentsFromPpv(uids)
