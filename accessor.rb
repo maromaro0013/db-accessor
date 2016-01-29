fea9e87665b5f802bd923066c90f2268a570124d
y-ohta <y-ohta@administraotr-no-MacBook-Air.local>
2016-01-29 10:16:42 +0900

    購入金額の合計だけ出すところまで進んだ

--- accessor.rb
+++ accessor.rb
@@ -2,6 +2,7 @@ require 'yaml'
 require "net/ssh/gateway"
 require "mysql2"
 require "active_record"
+require 'pp'
 
 @db = YAML.load_file('./database.yml')
 contents = YAML.load_file('./contents.yml')
@@ -20,11 +21,16 @@ class AdcodeSubscribeLasts < ActiveRecord::Base
   self.table_name = 'adcode_subscribe_lasts'
   self.readonly
 end
+class AnalyzePpvAll < ActiveRecord::Base
+  self.table_name = 'analyze_ppv_all'
+  self.readonly
+end
+
+=begin
 class Ppv < ActiveRecord::Base
   self.table_name = 'ppv'
   self.readonly
 end
-
 def getPaymentsFromPpv(uids)
   gateway = Net::SSH::Gateway.new('nrdb-01.gbnet', 'root', :port => 22, :password => ENV["ROOT_PASSWD"])
   if !gateway.active?
@@ -37,31 +43,20 @@ def getPaymentsFromPpv(uids)
     @db["nrdb-01.gbnet"]["database"] = "haru"
     ActiveRecord::Base.establish_connection(@db["nrdb-01.gbnet"])
 
-=begin
-    uid = "https://id.my.softbank.jp/service/idp/server.php/idpage?id=e4sb2boukr3r7crsa5lj8sarkcb1r34b3e676m16i6du1t9t9f0i2doah4nefstj"
-    q = Ppv.where(uid: uid)
-    puts q.count
-=end
-
-  q = Ppv.arel_table[:uid]
-  uid_sel = q.matches("")
-  uids.each {|uid|
-    uid_sel = uid_sel.or(q.matches(uid))
-    #q = q.or(q.where(uid: uid))
-  }
-  #uid_sel = q.matches(uid)
-  ret = Ppv.where(startdate: @date_range).where(uid_sel).pluck(:menuid)
-  puts ret.length
-=begin
-    q = Ppv.where(uid: "")
+    q = Ppv.arel_table[:uid]
+    uid_sel = q.matches("")
     uids.each {|uid|
-      q = q.or(q.where(uid: uid))
+      uid_sel = uid_sel.or(q.matches(uid))
     }
-    puts q.count
-=end
+    ret = Ppv.where(startdate: @date_range).where(uid_sel).pluck(:menuid)
+
+    payment_amount = 0
+    ret.each
+    puts ret.length
   }
   gateway.shutdown!
 end
+=end
 
 gateway.open("localhost", 3306) {|local_port|
   @db = YAML.load_file('./database.yml')
@@ -70,13 +65,34 @@ gateway.open("localhost", 3306) {|local_port|
   ActiveRecord::Base.establish_connection(@db["sadb-01"])
 
   adcode_uids = {}
+  adcode_payments = {}
+  adcode_amount = {}
+
   adcords.each {|adcord|
     adcode_uids[adcord] = []
     q = AdcodeSubscribeLasts.where(date: @date_range).where("adcode1 like ?", adcord)
     adcode_uids[adcord] = q.pluck(:uid)
+    #puts adcode_uids[adcord].length
+
+    adcode_payments[adcord] = []
+    adcode_amount[adcord] = 0
   }
-  #puts adcode_uids["pr=menulistad"].length
-  getPaymentsFromPpv(adcode_uids["pr=menulistad"])
+
+  adcords.each {|adcord|
+    adcode_uids[adcord].each {|uid|
+      ret = AnalyzePpvAll.where(start_date: @date_range).where("uid = ?", uid).select(:uid, :site_id, :menuid, :start_date, :charge)
+      if ret.length > 0
+        #adcode_payments[adcord] << ret
+        ret.each{|r|
+          adcode_amount[adcord] += r.charge
+        }
+      end
+    }
+  }
+
+  adcode_amount.each {|amt|
+    puts amt
+  }
 }
 
 gateway.shutdown!
