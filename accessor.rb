eefbdc7e04fb5e576e3fc1f6179482bd84c7a20a
y-ohta <y-ohta@administraotr-no-MacBook-Air.local>
2016-02-03 10:43:10 +0900

    ticket10277

--- accessor.rb
+++ accessor.rb
@@ -5,9 +5,8 @@ require "active_record"
 require 'pp'
 
 @db = YAML.load_file('./database.yml')
-contents = YAML.load_file('./contents.yml')
 adcords = YAML.load_file('./adcords.yml')
-@date_range = range = Time.new(2015, 12, 1, 0, 0, 0)..Time.new(2015, 12, 31, 23, 59, 59)
+@date_range = range = Time.new(2015, 11, 1, 0, 0, 0)..Time.new(2015, 11, 31, 23, 59, 59)
 
 adcode_uids = adcords
 
@@ -26,38 +25,6 @@ class AnalyzePpvAll < ActiveRecord::Base
   self.readonly
 end
 
-=begin
-class Ppv < ActiveRecord::Base
-  self.table_name = 'ppv'
-  self.readonly
-end
-def getPaymentsFromPpv(uids)
-  gateway = Net::SSH::Gateway.new('nrdb-01.gbnet', 'root', :port => 22, :password => ENV["ROOT_PASSWD"])
-  if !gateway.active?
-    p "connection failed"
-    return
-  end
-
-  gateway.open("localhost", 3306) {|local_port|
-    @db["nrdb-01.gbnet"]["port"] = local_port
-    @db["nrdb-01.gbnet"]["database"] = "haru"
-    ActiveRecord::Base.establish_connection(@db["nrdb-01.gbnet"])
-
-    q = Ppv.arel_table[:uid]
-    uid_sel = q.matches("")
-    uids.each {|uid|
-      uid_sel = uid_sel.or(q.matches(uid))
-    }
-    ret = Ppv.where(startdate: @date_range).where(uid_sel).pluck(:menuid)
-
-    payment_amount = 0
-    ret.each
-    puts ret.length
-  }
-  gateway.shutdown!
-end
-=end
-
 gateway.open("localhost", 3306) {|local_port|
   @db = YAML.load_file('./database.yml')
   @db["sadb-01"]["port"] = local_port
@@ -72,8 +39,6 @@ gateway.open("localhost", 3306) {|local_port|
     adcode_uids[adcord] = []
     q = AdcodeSubscribeLasts.where(date: @date_range).where("adcode1 like ?", adcord)
     adcode_uids[adcord] = q.pluck(:uid)
-    #puts adcode_uids[adcord].length
-
     adcode_payments[adcord] = []
     adcode_amount[adcord] = 0
   }
@@ -82,7 +47,6 @@ gateway.open("localhost", 3306) {|local_port|
     adcode_uids[adcord].each {|uid|
       ret = AnalyzePpvAll.where(start_date: @date_range).where("uid = ?", uid).select(:uid, :site_id, :menuid, :start_date, :charge)
       if ret.length > 0
-        #adcode_payments[adcord] << ret
         ret.each{|r|
           adcode_amount[adcord] += r.charge
         }
